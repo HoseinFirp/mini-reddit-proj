14a70fe1fe0accb2f466361ccaf207cea0ab1f30
HosseinFirp <hpp.021444@yahoo.com>
2024-03-10 17:58:51 +0330

    create post form

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -10,6 +10,7 @@ import {
 } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { useFormState } from "react-dom";
+import FormButton from "../common/form-button";
 
 export default function TopicCreateForm() {
   const [formState, action] = useFormState(actions.createTopic, { errors: {} });
@@ -26,7 +27,7 @@ export default function TopicCreateForm() {
               name="name"
               label="Name"
               labelPlacement="outside"
-              placeholder="Your name"
+              placeholder="Enter topic name"
               isInvalid={!!formState.errors.name}
               errorMessage={formState.errors.name?.join(", ")}
             />
@@ -44,7 +45,7 @@ export default function TopicCreateForm() {
                 {formState.errors._form.join(", ")}
               </div>
             ) : null}
-            <Button type="submit">Submit</Button>
+            <FormButton>Save</FormButton>
           </div>
         </form>
       </PopoverContent>
