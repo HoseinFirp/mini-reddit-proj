cb189071703042a235f8c6896361cabab233c643
HosseinFirp <hpp.021444@yahoo.com>
2024-03-10 10:53:16 +0330

    setting up app

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+// We create this when ever need outside servers to access our app
+export {GET,POST} from '@/auth'
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+
+export default function Home() {
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <Button type="submit">Click me !!</Button>
+      </form>
+    </div>
+  );
+}
